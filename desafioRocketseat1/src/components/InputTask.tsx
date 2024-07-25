39b91b9a92f1ddfa8fd97b74d30c1aa6fe4e06f3
Guilherme Reis <guilherme.reis@tripservice.com.br>
2024-07-25 00:14:32 -0300

    criando estrutura do projeto

---
+++ desafioRocketseat1/src/components/InputTask.tsx
@@ -0,0 +1,22 @@
+import styles from "./InputTask.module.css";
+import ToDoLogo from "../assets/ToDoLogo.svg";
+
+export function InputTask() {
+  return (
+    <div className={styles.coreDiv}>
+      <div className={styles.headerDiv}>
+        <img src={ToDoLogo} alt="Logo" className={styles.todoLogo} />{" "}
+        <span className={styles.spanTo}>to</span>
+        <span className={styles.spanDo}>do</span>
+      </div>
+
+      <div className={styles.inputItems}>
+        <input
+          placeholder="Adicione uma tarefa"
+          className={styles.typingTask}
+        />
+        <button className={styles.createButton}>Criar</button>
+      </div>
+    </div>
+  );
+}
---
+++ desafioRocketseat1/src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { InputTask } from "./components/InputTask";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <InputTask />
+  </React.StrictMode>
+);
